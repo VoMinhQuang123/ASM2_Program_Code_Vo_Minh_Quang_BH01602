232c56a8acdecee1a30a6c27d97d91cfc691d092
VoMinhQuang <158266479+VoMinhQuang123@users.noreply.github.com>
2024-04-06 22:16:31 +0700

    Create Pay.cs

---
+++ Pay.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp10
+{
+    public partial class PayPage : Form
+    {
+        public PayPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to exit the program?!", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
